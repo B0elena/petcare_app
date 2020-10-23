66ad92420a76fed4608e0fe6f591b8d791d65ff3
B0elena <e.mc2.1012md@gmail.com>
2020-10-23 13:54:30 +0900

    ログイン機能実装

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -1,5 +1,13 @@
 class PetsController < ApplicationController
+  before_action :move_to_index, except: [:index, :show]
   def index
     # @tweets = Tweet.all
+  end
+
+  private
+  def move_to_index
+    unless user_signed_in?
+      redirect_to root_path
+    end
   end
 end
