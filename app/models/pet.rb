0dd46b61fa0ad35fed829f3169b38fa4f6b56a23
B0elena <e.mc2.1012md@gmail.com>
2020-10-25 23:17:27 +0900

    修正

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -4,6 +4,6 @@ class Pet < ApplicationRecord
   has_one_attached :image
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :sex
-  validates :petname, :sex_id, :kind, :birthday, presence: true
+  validates :petname, :sex_id, :kind, :birthday, :image, presence: true
   validates :sex_id, numericality: { other_than: 1 } 
 end
